6e583a9207d0f06e7eb046007a88428c7e3d356d
Jacob Walker <jacob.l.walker.14@gmail.com>
2024-04-17 21:37:33 -0500

    wip

--- CalculateAxisStepValue/__tests__/calculateAxisStepValue.test.js
+++ CalculateAxisStepValue/__tests__/calculateAxisStepValue.test.js
@@ -1,9 +1,19 @@
 const { calculateAxisStepValue } = require('../calculateAxisStepValue');
 
 describe('calculateAxisStepValue', () => {
+  it('throws an error, when given a string', () => {
+    expect(() =>  calculateAxisStepValue(' ')).toThrow();
+  });
+
   it('returns 10, when given 100', () => {
     const result = calculateAxisStepValue(100);
 
     expect(result).toEqual(10);
+  });
+
+  it('returns 20, when given 200', () => {
+    const result = calculateAxisStepValue(200);
+
+    expect(result).toEqual(20);
   });
 });
--- CalculateAxisStepValue/calculateAxisStepValue.js
+++ CalculateAxisStepValue/calculateAxisStepValue.js
@@ -1,5 +1,13 @@
-function calculateAxisStepValue() {
-  return 0;
+function calculateAxisStepValue(maxChartValue) {
+  if(typeof maxChartValue !== 'number') {
+    throw new Error('Invalid maxChartValue for calculateAxisStepValue - maxChartValue must be a number.');
+  }
+
+  if(maxChartValue > 100) {
+    return 20;
+  }
+
+  return 10;
 }
 
 module.exports = { calculateAxisStepValue };
